973179fb312d1267650be0d727efd3fe09e4d142
eri <ek123sp@gmail.com>
2019-12-12 04:42:38 +0000

    課題提出Laravel12

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,19 @@ Route::group(['prefix'=>'admin'],function() {
 
 //課題３　「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。
 
-Route::get('XXX','AAAController@bbb');
 
 //【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください。
 
-Route::get('admin/profile/create','Admin\ProfileController@add');
+Route::get('admin/profile/create','Admin\ProfileController@add')->middleware('auth');
 
-Route::get('admin/profile/edit','Admin\ProfileController@edit');
+Route::get('admin/profile/edit','Admin\ProfileController@edit')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+   
+});
